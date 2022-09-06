ed6781f546a6a9cde0d91a41883350f29b0f6af7
yochidros <yoshiki.myzw@gmail.com>
2022-09-06 21:35:51 +0900

    add: project

--- Sources/ActomatonSample/ActomatonSample.swift
+++ Sources/ActomatonSample/ActomatonSample.swift
@@ -1,6 +1,46 @@
-public struct ActomatonSample {
-    public private(set) var text = "Hello, World!"
+import Actomaton
+import ActomatonStore
 
-    public init() {
+public enum Logic {
+    struct State: Equatable, Sendable {
+        var count: Int
+    }
+
+    enum Action: Sendable {
+        case tap
+    }
+
+    struct _Environment: Sendable {
+        let increment: @Sendable () -> Void
+    }
+
+    typealias Store = ActomatonStore.Store<Action, State, _Environment>
+    typealias Environment = _Environment
+
+    static func reducer() -> Reducer<Action, State, Environment> {
+        .init { action, state, environment in
+            switch action {
+            case .tap:
+                return Effect {
+                    return nil
+                } + Effect {
+                    return nil
+                } + Effect {
+                    return nil
+                } + Effect {
+                    return nil
+                } + Effect {
+                    return nil
+                } + Effect {
+                    return nil
+                } + Effect {
+                    return nil
+                } + Effect {
+                    return nil
+                } + Effect {
+                    return nil
+                }
+            }
+        }
     }
 }
